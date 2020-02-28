aa0002f7a64d6db863c1cd8a630f00ee74ea7dc1
Rafael Meneses <rafael.meneses@tuneduc.com.br>
2020-02-28 11:29:46 -0300

    Create row rendering from ERB template

--- spec/lib/csv_to_html/row_spec.rb
+++ spec/lib/csv_to_html/row_spec.rb
@@ -3,18 +3,30 @@ require 'csv'
 require 'csv_to_html/row'
 
 RSpec.describe CsvToHtml::Row do
+  let!(:csv) { CSV.read 'spec/fixtures/people.csv', headers: true }
+  let!(:csv_row) { csv[0] }
+
+  subject { CsvToHtml::Row.new(csv_row) }
+
   describe '#initialize' do
-    let!(:csv) { CSV.read 'spec/fixtures/people.csv', headers: true }
-    let!(:csv_row) { csv[0] }
-
-    subject { CsvToHtml::Row.new(csv_row) }
-
     it 'creates methods from columns' do
       expect(subject.public_methods).to include :id
       expect(subject.public_methods).to include :name
 
       expect(subject.id).to eq csv_row['id']
       expect(subject.name).to eq csv_row['name']
+    end
+  end
+
+  describe '#render' do
+    let!(:html) { 'The id is <%= id %> and the name is <%= name %>' }
+    let!(:template) { ERB.new(html) }
+    let!(:output) do
+      "The id is #{csv_row['id']} and the name is #{csv_row['name']}"
+    end
+
+    it 'renders ERB with row binding' do
+      expect(subject.render(template)).to eq output
     end
   end
 end
