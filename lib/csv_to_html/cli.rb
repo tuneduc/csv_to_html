05c3e60ffccf8974fc19692d30f33d43299c9947
Rafael Meneses <rafael.meneses@tuneduc.com.br>
2020-02-28 16:42:13 -0300

    Generate files from template

--- lib/csv_to_html/cli.rb
+++ lib/csv_to_html/cli.rb
@@ -1,5 +1,7 @@
 require 'thor'
 require 'erb'
+require 'csv'
+require 'csv_to_html/row'
 
 module CsvToHtml
   class CLI < Thor
@@ -13,11 +15,17 @@ module CsvToHtml
       raise Thor::Error, 'ERB template file not found!' \
         unless File.exist? erb_path
 
-      raise Thor::Error, 'CSV file not found!' \
-        unless File.exist? csv_path
+      raise Thor::Error, 'CSV file not found!' unless File.exist? csv_path
 
       raise Thor::Error, 'Output path not a directory!' \
         unless Dir.exist? output_path
+
+      erb = ERB.new File.read(erb_path)
+
+      CSV.foreach(csv_path, headers: true).with_index(1) do |row, i|
+        row_output = CsvToHtml::Row.new(row).render(erb)
+        File.write "#{output_path}/#{i}.html", row_output
+      end
     end
   end
 end
--- spec/lib/csv_to_html/cli_spec.rb
+++ spec/lib/csv_to_html/cli_spec.rb
@@ -8,9 +8,11 @@ RSpec.describe CsvToHtml::CLI do
   end
 
   describe '#build' do
+    let!(:fixtures_path) { File.expand_path '../../fixtures', __dir__ }
+    let!(:erb_path) { "#{fixtures_path}/person.html.erb" }
+    let!(:csv_path) { "#{fixtures_path}/people.csv" }
+
     context 'Inexistent files' do
-      let!(:erb_path) { 'spec/fixtures/person.html.erb' }
-      let!(:csv_path) { 'spec/fixtures/people.csv' }
       let!(:wrong_erb_path) { 'spec/this/is/not/a/file' }
       let!(:wrong_csv_path) { 'spec/this/is/not/a/file' }
       let!(:wrong_output_path) { 'spec/this/is/not/a/dir' }
@@ -30,6 +32,34 @@ RSpec.describe CsvToHtml::CLI do
       it 'raises error for wrong csv path' do
         expect { subject.build(erb_path, csv_path, wrong_output_path) }.to \
           raise_error Thor::Error, 'Output path not a directory!'
+      end
+    end
+
+    context 'Existent files' do
+      include FakeFS::SpecHelpers
+
+      let!(:output_path) { '/home/output' }
+      let(:file_list) { Dir.glob "#{output_path}/*" }
+
+      before do
+        FakeFS::FileSystem.clone fixtures_path
+        FileUtils.mkdir_p output_path
+
+        subject.build(erb_path, csv_path, output_path)
+      end
+
+      it 'creates files in desired path' do
+        expect(file_list.size).to eq 3
+        expect(file_list).to include "#{output_path}/1.html"
+        expect(file_list).to include "#{output_path}/2.html"
+        expect(file_list).to include "#{output_path}/3.html"
+      end
+
+      it 'generates correct output' do
+        file_list.each do |output|
+          expect(File.read(output)).to eq \
+            File.read("#{fixtures_path}/output/#{File.basename(output)}")
+        end
       end
     end
   end
