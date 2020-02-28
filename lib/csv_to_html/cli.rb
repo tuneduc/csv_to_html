195cf487be476bac16f9254ce7620b39ed1447bc
Rafael Meneses <rafael.meneses@tuneduc.com.br>
2020-02-28 14:27:57 -0300

    Add build method with input validation

--- lib/csv_to_html/cli.rb
+++ lib/csv_to_html/cli.rb
@@ -1,9 +1,23 @@
 require 'thor'
+require 'erb'
 
 module CsvToHtml
   class CLI < Thor
     def self.exit_on_failure?
       true
+    end
+
+    desc 'build ERB_TEMPLATE CSV OUTPUT_PATH',
+         'Build files from the ERB template and CSV file'
+    def build(erb_path, csv_path, output_path)
+      raise Thor::Error, 'ERB template file not found!' \
+        unless File.exist? erb_path
+
+      raise Thor::Error, 'CSV file not found!' \
+        unless File.exist? csv_path
+
+      raise Thor::Error, 'Output path not a directory!' \
+        unless Dir.exist? output_path
     end
   end
 end
--- spec/lib/csv_to_html/cli_spec.rb
+++ spec/lib/csv_to_html/cli_spec.rb
@@ -6,4 +6,31 @@ RSpec.describe CsvToHtml::CLI do
       expect(described_class.exit_on_failure?).to eq true
     end
   end
+
+  describe '#build' do
+    context 'Inexistent files' do
+      let!(:erb_path) { 'spec/fixtures/person.html.erb' }
+      let!(:csv_path) { 'spec/fixtures/people.csv' }
+      let!(:wrong_erb_path) { 'spec/this/is/not/a/file' }
+      let!(:wrong_csv_path) { 'spec/this/is/not/a/file' }
+      let!(:wrong_output_path) { 'spec/this/is/not/a/dir' }
+
+      it 'raises error for wrong erb path' do
+        expect do
+          subject.build(wrong_erb_path, wrong_csv_path, wrong_output_path)
+        end.to raise_error Thor::Error, 'ERB template file not found!'
+      end
+
+      it 'raises error for wrong csv path' do
+        expect do
+          subject.build(erb_path, wrong_csv_path, wrong_output_path)
+        end.to raise_error Thor::Error, 'CSV file not found!'
+      end
+
+      it 'raises error for wrong csv path' do
+        expect { subject.build(erb_path, csv_path, wrong_output_path) }.to \
+          raise_error Thor::Error, 'Output path not a directory!'
+      end
+    end
+  end
 end
